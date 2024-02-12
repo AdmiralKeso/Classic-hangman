665f37b38e7cdd6f90781a65b35e9b11eced42fb
Cas Hagenaar <you@example.com>
2024-02-12 12:24:45 +0100

    Add global variables

--- run.py
+++ run.py
@@ -1,3 +1,90 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Global variable for tries
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+#Global variable for words
+words = ('cat dog tiger gorilla giraffe mouse beaver camel' 
+         'coyote wolf rat ant panther puma aardvark bear' 
+         'lynx fox elephant swan duck shark badger').split()
+#Global variable for guessed
+guessed = False
+#Global variable for guessed letters
+guessed_letters = []
+#Global variable for guessed word
+guessed_word = []
+#Global variable for tries
+tries = 6
